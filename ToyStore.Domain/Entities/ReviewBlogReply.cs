ed4eead3c6846b592a6b293365702dc4507b5f48
luanlee08 <luanlevn2@gmail.com>
2026-05-21 13:15:44 +0700

    Refactor: chỉnh sửa model cho chức năng AI

--- ToyStore.Domain/Entities/ReviewBlogReply.cs
+++ ToyStore.Domain/Entities/ReviewBlogReply.cs
@@ -17,6 +17,20 @@ public partial class ReviewBlogReply
 
     public string Comment { get; set; } = null!;
 
+    public string ModerationStatus { get; set; } = null!;
+
+    public DateTime? ManualReviewDeadline { get; set; }
+
+    public byte RetryCount { get; set; }
+
+    public DateTime? LastRetryAt { get; set; }
+
+    public bool IsHidden { get; set; }
+
+    public int? HiddenBy { get; set; }
+
+    public DateTime? HiddenAt { get; set; }
+
     public bool IsDeleted { get; set; }
 
     public DateTime CreatedAt { get; set; }
@@ -25,6 +39,8 @@ public partial class ReviewBlogReply
 
     public virtual Account Account { get; set; } = null!;
 
+    public virtual Account? HiddenByNavigation { get; set; }
+
     public virtual ICollection<ReviewBlogReply> InverseParentReply { get; set; } = new List<ReviewBlogReply>();
 
     public virtual ReviewBlogReply? ParentReply { get; set; }
@@ -32,6 +48,8 @@ public partial class ReviewBlogReply
     public virtual Account? ReplyToAccount { get; set; }
 
     public virtual ReviewBlog ReviewBlog { get; set; } = null!;
+
+    public virtual ICollection<BlogCommentModerationLog> BlogCommentModerationLogs { get; set; } = new List<BlogCommentModerationLog>();
 
     public virtual ICollection<ReviewBlogReplyReaction> ReviewBlogReplyReactions { get; set; } = new List<ReviewBlogReplyReaction>();
 }
